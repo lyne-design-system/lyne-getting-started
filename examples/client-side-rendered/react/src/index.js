e1a7e0156a05c4c168cb5481f8a9183ce4ca5bb4
Yves Tscherry <info@feerglas.com>
2021-08-17 16:21:06 +0200

    fix: correctly import loader

--- examples/client-side-rendered/react/src/index.js
+++ examples/client-side-rendered/react/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { defineCustomElements } from 'lyne-test/loader';
+import { defineCustomElements } from 'lyne-test/dist/esm/loader';
 
 defineCustomElements();
 
